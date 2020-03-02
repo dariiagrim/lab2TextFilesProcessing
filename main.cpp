ffa464d5b4a052d930725caf8ab0fde93b6b916d
Andrii Moisol <andrii.moisol@gmail.com>
2020-03-02 19:05:41 +0200

    Split data & create countries array

--- main.cpp
+++ main.cpp
@@ -4,9 +4,23 @@
 #include <algorithm>
 #include <map>
 #include <experimental/filesystem>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 namespace fs = std::experimental::filesystem;
+
+vector<std::string> split(const string& s, char delimiter)
+{
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(s);
+    while (getline(tokenStream, token, delimiter))
+    {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
 
 int main() {
     string directoryName;
@@ -21,8 +35,28 @@ int main() {
         if (entry.path().extension().string() == ".csv")
             files.push_back(entry.path().string());
     }
-    for (string file : files) {
-        cout << file << endl;
+
+    vector<string> countries;
+
+    for (const string& fileName : files) {
+        ifstream file;
+        file.open(fileName);
+
+        int n;
+        file >> n;
+
+        for (int i = 0; i < n; i++) {
+            string s;
+            getline(file >> ws, s);
+            vector<string> data = split(s, ',');
+            countries.push_back(data[0]);
+        }
+
+        file.close();
+    }
+
+    for (auto country : countries) {
+        cout << country << endl;
     }
 
     return 0;
