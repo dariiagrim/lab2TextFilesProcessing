610363d851f933e4bbc79409aaeed8062cafbeb0
Andrii Moisol <andrii.moisol@gmail.com>
2020-03-02 18:30:44 +0200

    Reading directory

--- main.cpp
+++ main.cpp
@@ -3,15 +3,23 @@
 #include <string>
 #include <algorithm>
 #include <map>
+#include <filesystem>
 
 using namespace std;
+namespace fs = std::filesystem;
 
 int main() {
     string directoryName;
 
-    cout << "Please, write name of directory: ";
+    cout << "Please, write path to directory: ";
 
     cin >> directoryName;
+
+    vector<string> files;
+
+    for (const auto& entry : fs::directory_iterator(directoryName)) {
+        cout << entry.path().string() << endl;
+    }
 
     return 0;
 }
