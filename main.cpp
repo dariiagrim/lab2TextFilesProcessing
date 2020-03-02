6a1b861c60381d42265e8a361affca8a9258797f
Dasha <grimalskayad@gmail.com>
2020-03-02 17:48:14 +0200

    Include libraries

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
